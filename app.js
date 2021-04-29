ba06a03322de1b5f348cc122bf8d7a837cb09f51
Nukareddy Sivateja Reddy <sivatejareddy.nukareddy@gmail.com>
2021-04-29 16:00:11 +0530

    Mongoose connection added

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 3200;
 
+
+//Imports
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+
+//App Set Up
+const PORT = process.env.PORT || 3200;
 const app = express();
 
-
-app.get('/', (req, res, next) => {
-    res.json({"message": "Thotakura kattamma is watching"});
+app.get("/", (req, res, next) => {
+  res.json({ message: "Thotakura kattamma is watching" });
 });
 
 
 
-app.listen(PORT, () => {
-    console.log('Thotakura kattamma is watching');
-})
+
+
+
+
+
+
+// Mongoose Set Up
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose
+  .connect('mongodb+srv://SivatejaN:Srm@9963956295@cluster0.m6kqz.mongodb.net/SWAGBAG?retryWrites=true&w=majority')
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Thotakura kattamma is watching");
+    });
+  })
+  .catch((error) => {
+    console.log("Mongo db Connection failed", error);
+  });
